fe0b40027b9f2333f33da3897876d89db1d9d50b
vladislav95 <noconquer@gmail.com>
2024-07-24 15:56:42 +0200

    implemented pre-commit

--- fast_api.py
+++ fast_api.py
@@ -12,21 +12,21 @@ class Task(BaseModel):
     done: bool = False
 
 
-CSV_FILE = 'todo.csv'
+CSV_FILE = "todo.csv"
 
 # Initialize CSV file with headers if it does not exist
 if not os.path.exists(CSV_FILE):
-    with open(CSV_FILE, mode='w', newline='') as file:
+    with open(CSV_FILE, mode="w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(['id', 'title', 'done'])
+        writer.writerow(["id", "title", "done"])
 
 
 @app.post("/tasks")
 def add_item(task: Task):
 
-    # Add a new task 
-    with open(CSV_FILE, mode='a', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=['id', 'title', 'done'])
+    # Add a new task
+    with open(CSV_FILE, mode="a", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=["id", "title", "done"])
         writer.writerow(task.model_dump())
 
     return {"message": "Task added successfully"}
@@ -36,8 +36,8 @@ def add_item(task: Task):
 def get_items():
     # Returning all rows from a database
     response = []
-    with open(CSV_FILE, mode='r', newline='') as file:
-        reader = csv.DictReader(file, fieldnames=['id', 'title', 'done'])
+    with open(CSV_FILE, newline="") as file:
+        reader = csv.DictReader(file, fieldnames=["id", "title", "done"])
         for row in reader:
             response.append(row)
 
@@ -48,8 +48,8 @@ def get_items():
 def get_item_by_title(title_to_get: str):
     # Returning all rows by title
     response = []
-    with open(CSV_FILE, mode='r', newline='') as file:
-        reader = csv.DictReader(file, fieldnames=['id', 'title', 'done'])
+    with open(CSV_FILE, newline="") as file:
+        reader = csv.DictReader(file, fieldnames=["id", "title", "done"])
         for row in reader:
             if title_to_get == row["title"]:
                 response.append(row)
@@ -61,42 +61,44 @@ def get_item_by_title(title_to_get: str):
 def update_item(title_id: str, task: Task):
     # Read existing items
     items = []
-    with open(CSV_FILE, mode='r', newline='') as file:
+    with open(CSV_FILE, newline="") as file:
         reader = csv.DictReader(file)
         for row in reader:
             items.append(row)
 
     # Find and update the item
     for row in items:
-        if str(row['title']) == title_id:
-            row['title'] = task.title
-            row['id'] = task.id
-            row['done'] = task.done
+        if str(row["title"]) == title_id:
+            row["title"] = task.title
+            row["id"] = task.id
+            row["done"] = task.done
 
-        with open(CSV_FILE, mode='w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'done'])
+        with open(CSV_FILE, mode="w", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "done"])
             writer.writeheader()
             writer.writerows(items)
 
     return "updated succusful"
 
+
 @app.delete("/tasks/{task_name}")
 def delete_task(task_name: str):
     tasks = []
-    with open(CSV_FILE, mode='r', newline='') as file:
+    with open(CSV_FILE, newline="") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            if str(row['title']) != task_name:
+            if str(row["title"]) != task_name:
                 tasks.append(row)
 
-    with open(CSV_FILE, mode='w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=['id', 'title', 'done'])
+    with open(CSV_FILE, mode="w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=["id", "title", "done"])
         writer.writeheader()
         writer.writerows(tasks)
 
     return {"message": "Task deleted successfully"}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
